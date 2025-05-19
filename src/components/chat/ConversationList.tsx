1fc07c4fc3bb3b23e7b5f67de8fc5daaed507df9
yhonaiker moncada <yhonaiker1@gmail.com>
2025-05-19 13:46:32 -0500

    feat: Enhance ConversationList component with props for conversations,

--- src/components/chat/ConversationList.tsx
+++ src/components/chat/ConversationList.tsx
@@ -1,1 +1,12 @@
-export const ConversationList = () => null
+import { Conversation } from '@/lib/types/chat'
+
+interface ConversationListProps {
+  conversations: Conversation[]
+  currentConversationId: string | null
+  onCreateNewChat: () => void
+  onSelectConversation: (id: string) => void
+}
+
+export const ConversationList = (props: ConversationListProps) => {
+  return null
+}
